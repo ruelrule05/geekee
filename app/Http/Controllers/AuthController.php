a2b23aa42498a53f0c6c01268c0a34e668760163
Ruel Rule <ruelrule05@gmail.com>
2022-03-20 10:21:32 +0800

    login and logout APIs ready

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{    
+    /**
+     * login
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user)
+        {
+            if (Hash::check($request->password, $user->password))
+            {
+                return response()->json([
+                    'success'           =>  true,
+                    'token'             =>  $user->createToken($request->device_name)->plainTextToken
+                ]);
+            }
+        }
+
+        return response()->json([
+            'success'       =>  false,
+            'message'       =>  'Invalid credentials.'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $logout = $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'success'       =>  $logout,
+            'message'       =>  'You have been logged out.'
+        ]);
+    }
+}
